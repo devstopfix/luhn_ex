891d237007deec513bbcc30e94572002852091f9
J Every <devstopfix@gmail.com>
2022-11-30 17:34:08 +0000

    refute invalid

--- test/luhn_test.exs
+++ test/luhn_test.exs
@@ -5,35 +5,35 @@ defmodule LuhnTest do
     assert Luhn.valid? "378282246310005"
     assert Luhn.valid? "371449635398431"
   end
-  
+
   test "American Express Corporate" do
     assert Luhn.valid? "378734493671000"
   end
-  
+
   test "Australian BankCard" do
     assert Luhn.valid? "5610591081018250"
   end
-  
+
   test "Diners Club" do
     assert Luhn.valid? "30569309025904"
     assert Luhn.valid? "38520000023237"
   end
-  
+
   test "Discover" do
     assert Luhn.valid? "6011111111111117"
     assert Luhn.valid? "6011000990139424"
   end
-  
+
   test "JCB" do
     assert Luhn.valid? "3530111333300000"
     assert Luhn.valid? "3566002020360505"
   end
-  
+
   test "MasterCard" do
     assert Luhn.valid? "5555555555554444"
     assert Luhn.valid? "5105105105105100"
   end
-  
+
   test "Visa" do
     assert Luhn.valid? "4111111111111111"
     assert Luhn.valid? "4012888888881881"
@@ -45,9 +45,9 @@ defmodule LuhnTest do
   end
 
   test "Invalid numbers" do
-    assert !Luhn.valid? "123456789123456"
-    assert !Luhn.valid? 123456789123456
-    assert !Luhn.valid? "4111111511111112"
+    refute Luhn.valid? "123456789123456"
+    refute Luhn.valid? 123456789123456
+    refute Luhn.valid? "4111111511111112"
   end
 
   test "Hexadecimal base" do
@@ -56,7 +56,7 @@ defmodule LuhnTest do
   end
 
   test "Invalid with hexadecimal base" do
-    assert !Luhn.valid? "abc12392b", 16
-    assert !Luhn.valid? "abc123a", 16
+    refute Luhn.valid? "abc12392b", 16
+    refute Luhn.valid? "abc123a", 16
   end
 end
